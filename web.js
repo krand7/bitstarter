26e13a4dbff99f964e60e0ee84b81d53120970f3
Kyle Rand <krand7@gmail.com>
2013-07-11 03:12:02 +0000

    Fixed to use buffer as a variable name

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync(index.html,encoding);
+var indexbuffer = fs.readFileSync('index.html');
+
+indexbuffer.toString('utf-8')
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2');
+  response.send(indexbuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;

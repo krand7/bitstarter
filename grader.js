ea63b41294c521d11e4e8e0bf155cd590cc62dd2
Kyle Rand <krand7@gmail.com>
2013-07-12 03:56:21 +0000

    Adjusted for hw3 submission.

--- grader.js
+++ grader.js
@@ -5,7 +5,6 @@ Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
 
 References:
-
  + cheerio
    - https://github.com/MatthewMueller/cheerio
    - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
@@ -24,6 +23,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +36,21 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+//var assertURLExists = function(infile) {
+ //   var instr = infile.toString();
+  //  rest.get('instr').on('complete', function(result) {
+//	if (result instanceof Error) {
+//	    console.error("%s does not exist. Exiting.", instr);
+//	    process.exit(1);
+//	}
+//	else {
+//	    console.error("%s was written.", instr);
+//	    fs.writeFileSync(instr,result);
+//	}
+  //  });
+//};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -55,6 +71,57 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var proceed = 0;
+
+var cheerioURLFile = function(url_name, checksfile,  callback) {
+    var instr=url_name.toString();   //redudant
+    rest.get(instr).on('complete', function(result) {
+	if (result instanceof Error) {
+	    console.log("%s cannot be found: %s", instr, result.message);
+	    process.exit(1);
+	} 
+	else {
+	    console.log('$ is about to be called');
+	    callback(result);
+	    console.log('$ is called, so continue to next section');
+	    //return result;
+	}
+	console.log('This makes it here');
+	var checks=loadChecks(checksfile).sort();
+	console.log(checks)
+	$ = cheerio.load(result);
+	var out={};
+	for (var ii in checks) {
+	    var present=$(checks[ii]).length>0;
+	    out[checks[ii]]=present;
+	}
+	console.log(out);
+	return out;
+    });
+};
+
+var checkURL = function(url_name, checksfile) {
+    $ = cheerioURLFile(url_name, checksfile, function (result) {console.log(result);});
+    console.log('$ is being called');
+    //var checks = loadChecks(checksfile).sort();
+    //var out = {};
+    //for (var ii in checks) {
+//	var present = $(checks[ii]).length >0;
+//	out[checks[ii]] = present;
+  //  }
+    //return out;
+};
+
+var checkURL2 = function(url_name, checksfile) {
+    var checks=loadChecks(checksfile).sort();
+    var out={};
+    for (var ii in checks) {
+	var present = $(checks[ii]).length>0;
+	out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,9 +132,12 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_address>', 'Path to checks.json', 'A string', HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    //var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson = checkURL(program.url, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
+    //outJson > outJson.txt;
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
